f87c291ce7ee7a54a987f3d8bd1a43e1cee2b6a0
Todd DeLuca <no-spam@todddeluca.com>
2012-05-29 10:38:45 -0400

    Fix pip installation failure involving README.md
    Two bugs with "easy" fixes:
    
    - README.md was not being included in the source distribution.  I'm not
      sure what I did to fix it, since the distutils/setuptools/distribute
      docs are quite incomplete and convoluted on something so
      straight-forward.  The fix: I removed some 'package_data' type lines
      from setup.py, and now LICENSE.txt and README.md are being included.
    - In addition to README.md not being included in the distribution
      (tar.gz file) it was being read by setup.py as open('README.md'),
      which is relative to the current working directory, which I'm not sure
      pip sets to the directory containing setup.py.  The fix is to open the
      file using the directory of setup.py, via the __file__ attribute.

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 
-from setuptools import setup
+import os
+from setuptools import setup, find_packages
 
 setup(
     name = 'diabric',
-    version = '0.1',
+    version = '0.1.1',
     license = 'MIT',
     description = 'Diabolically atomic Python Fabric fabfile tasks and utilities.',
-    long_description = open('README.md').read(),
+    long_description = open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
     keywords = 'fabric fabfile boto ec2 virtualenv python wsgi webapp deployment',
     url = 'https://github.com/todddeluca/diabric',
     author = 'Todd Francis DeLuca',
@@ -16,9 +17,7 @@ setup(
                    'Programming Language :: Python :: 2',
                    'Programming Language :: Python :: 2.7',
                   ],
-    packages = ['diabric'],
     install_requires = ['setuptools', 'Fabric>=1.4','boto>=2.3'],
-    include_package_data = True,
-    package_data = {'' : ['README.md', 'LICENSE.txt']},
+    packages = ['diabric'],
 )
 
